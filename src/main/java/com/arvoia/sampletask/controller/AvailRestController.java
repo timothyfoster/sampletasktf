7762efaec65a8ba62764d2a85aac1a52d9190939
Tim Foster <timsprimary@gmail.com>
2020-01-27 13:40:25 +0200

    Use Jackson to map JSON onto vehicle and search models

--- src/main/java/com/arvoia/sampletask/controller/AvailRestController.java
+++ src/main/java/com/arvoia/sampletask/controller/AvailRestController.java
@@ -1,6 +1,8 @@
 package com.arvoia.sampletask.controller;
 
 import com.arvoia.sampletask.model.AvailResponse;
+import com.arvoia.sampletask.model.VehicleSearchResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration
 @RestController
@@ -21,11 +25,14 @@ public class AvailRestController {
   private String vendorUrl;
 
   @GetMapping(value = "/avail", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public @ResponseBody AvailResponse getAvailabilities() {
+  public @ResponseBody
+  AvailResponse getAvailabilities() throws IOException {
     AvailResponse ret = new AvailResponse();
     String searchResult = getSearchResults();
-    // TODO implement
-    ret.setResult(searchResult);
+    ObjectMapper objectMapper = new ObjectMapper();
+    VehicleSearchResult jsonSearchResult = objectMapper.readValue(searchResult, VehicleSearchResult.class);
+    String resultAsString = objectMapper.writeValueAsString(jsonSearchResult);
+    ret.setResult(resultAsString);
     return ret;
   }
 
