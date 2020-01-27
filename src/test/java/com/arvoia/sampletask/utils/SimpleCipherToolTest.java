e801cf3ae10578a1432dc094037e43770aad8bc3
Tim Foster <timsprimary@gmail.com>
2020-01-27 14:00:17 +0200

    Add tests for cipher tool

--- src/test/java/com/arvoia/sampletask/utils/SimpleCipherToolTest.java
+++ src/test/java/com/arvoia/sampletask/utils/SimpleCipherToolTest.java
@@ -1,29 +1,77 @@
 package com.arvoia.sampletask.utils;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-class SimpleCipherToolTest {
+import static org.junit.Assert.assertEquals;
 
-  private SimpleCipherTool underTest;
+@RunWith(SpringRunner.class)
+public class SimpleCipherToolTest {
 
-  @Test
-  public void testCipher() {
-    // given
-    // when
-    String actual = underTest.cipher("");
-    // then
-    assertNotNull(actual);
+  @Test(expected = IllegalArgumentException.class)
+  public void testExceptionThrownOnInvalidKeyWithCapitals() {
+    new SimpleCipherTool("AAAA");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExceptionThrownOnInvalidKeyWithNumbers() {
+    new SimpleCipherTool("aa9");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExceptionThrownOnInvalidKeyWithSymbols() {
+    new SimpleCipherTool("!@*#");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExceptionThrownOnInvalidKeyWithNullLength() {
+    new SimpleCipherTool("");
   }
 
   @Test
-  public void testDecipher() {
-    // given
-    // when
-    String actual = underTest.decipher("");
-    // then
-    assertNotNull(actual);
+  public void testEncipherWithSimpleKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("a");
+    assertEquals("bbbbb", simpleCipherTool.cipher("aaaaa"));
+    assertEquals("-+/*&", simpleCipherTool.cipher("-+/*&"));
+    assertEquals("d_d", simpleCipherTool.cipher("c_c"));
   }
 
+  @Test
+  public void testEncipherWithAbcKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("abc");
+    assertEquals("bcd", simpleCipherTool.cipher("aaa"));
+    assertEquals("b_d", simpleCipherTool.cipher("a_a"));
+    assertEquals("jln", simpleCipherTool.cipher("ijk"));
+  }
+
+  @Test
+  public void testEncipherWithArvoiaKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("arvoia");
+    assertEquals("ilpeb://cxcivt.rxn/uodacbrkar/tsieufusozcg",
+        simpleCipherTool.cipher("https://github.com/csorbazoli/sampletasktf"));
+  }
+
+  @Test
+  public void testDecipherWithSimpleKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("a");
+    assertEquals("aaaaa", simpleCipherTool.decipher("bbbbb"));
+    assertEquals("-+/*&", simpleCipherTool.decipher("-+/*&"));
+    assertEquals("c_c", simpleCipherTool.decipher("d_d"));
+  }
+
+  @Test
+  public void testDecipherWithAbcKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("abc");
+    assertEquals("aaa", simpleCipherTool.decipher("bcd"));
+    assertEquals("a_a", simpleCipherTool.decipher("b_d"));
+    assertEquals("ijk", simpleCipherTool.decipher("jln"));
+  }
+
+  @Test
+  public void testDecipherWithArvoiaKey() {
+    SimpleCipherTool simpleCipherTool = new SimpleCipherTool("arvoia");
+    assertEquals("https://github.com/csorbazoli/sampletasktf",
+        simpleCipherTool.decipher("ilpeb://cxcivt.rxn/uodacbrkar/tsieufusozcg"));
+  }
 }
